3907a94c84b4d0fe3288754932e9160756e56ed2
DEVENDRA <devendrakhanal006@gmail.com>
2026-02-16 17:23:12 +0545

    Update TO-DO App.py

--- TO-DO App.py
+++ TO-DO App.py
@@ -17,3 +17,18 @@ def add_task(tasks):
     tasks.append({"title": title, "done": False})
     save_tasks(tasks)
     print(" Task added!")
+ef view_tasks(tasks):
+    if not tasks:
+        print("No tasks yet.")
+        return
+    for i, task in enumerate(tasks, 1):
+        status = "✔" if task["done"] else "✘"
+        print(f"{i}. {task['title']} [{status}]")
+
+def complete_task(tasks):
+    view_tasks(tasks)
+    idx = int(input("Enter task number to mark complete: ")) - 1
+    if 0 <= idx < len(tasks):
+        tasks[idx]["done"] = True
+        save_tasks(tasks)
+        prin
