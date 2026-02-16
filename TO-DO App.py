0ea723d15ec806847982e39c3d5c8e1f29692234
DEVENDRA <devendrakhanal006@gmail.com>
2026-02-16 17:21:45 +0545

    Update TO-DO App.py

--- TO-DO App.py
+++ TO-DO App.py
@@ -1,1 +1,18 @@
 
+FILE = "tasks.json"
+
+def load_tasks():
+    if not os.path.exists(FILE):
+        return []
+    with open(FILE, "r") as f:
+        return json.load(f)
+
+def save_tasks(tasks):
+    with open(FILE, "w") as f:
+        json.dump(tasks, f, indent=2)
+
+def add_task(tasks):
+    title = input("Enter task: ")
+    tasks.append({"title": title, "done": False})
+    save_tasks(tasks)
+    print(" Task added!")
