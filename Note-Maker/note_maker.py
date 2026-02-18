3118ded2c9d71ae17fde45f7617da88446b7b0c3
DEVENDRA <devendrakhanal006@gmail.com>
2026-02-18 12:08:05 +0545

    Update note_maker.py

--- Note-Maker/note_maker.py
+++ Note-Maker/note_maker.py
@@ -1,1 +1,16 @@
-.
+import streamlit as st
+from openai import OpenAI
+from pypdf import PdfReader
+
+st.set_page_config(page_title="NoteCraft AI", page_icon="", layout="centered")
+
+client = OpenAI(api_key="") #your api key
+
+
+def read_pdf(file):
+    reader = PdfReader(file)
+    text = ""
+    for page in reader.pages:
+        text += page.extract_text() or ""
+    return text
+
