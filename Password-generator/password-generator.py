9cb08d61f450d7e657a2861ee4fecb5304af2f8d
DEVENDRA <devendrakhanal006@gmail.com>
2026-02-20 15:25:25 +0545

    Update password-generator.py

--- Password-generator/password-generator.py
+++ Password-generator/password-generator.py
@@ -10,3 +10,19 @@ class App:
         self.window.iconphoto(False, PhotoImage(file='')) #add your logo here
         self.window.geometry('500x255')
         self.window.config(bg='gray')
+         self.label()
+        self.entry()
+        self.button()
+
+    def label(self):
+        label_title = Label(self.window, text='Welcome to password generator', font=('Courrier', 20), bg='gray', fg='black')
+        label_title.pack()
+
+    def entry(self):
+        self.password_entry = Entry(self.window, font=('Courrier', 25), bg='white', fg='black', width=30, relief='solid')
+        self.password_entry.pack(pady=50)
+
+    def button(self):
+        password_generator = Button(self.window, text="Generate_password",  font=('Courrier', 12), bg='white', fg='black', width=25, command=self.generate_password)
+        password_generator.pack()
+
